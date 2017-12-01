b4ae6c00b5938d0a5bbc0b0a443094deb235abc4
morrowind1983 <morrowind1983@gmail.com>
2017-12-01 17:17:32 +0800

    Add Pro_4_1_3

---
+++ 02 Part Two - Data Structures/04 Chapter Four - Abstract Data Types/Program_4_1_3/Point.h
@@ -0,0 +1,26 @@
+/*
+Program 4.3. Point ADT interface
+
+By convention, we derive the interface associated with a class ADT implementation
+by removing the private parts and by replacing function implementations with
+declarations (signatures). This interface is derived in this way from Program
+4.1. We can use different implementations that have the same interface without
+changing any code in the client programs that use the ADT.
+*/
+
+#include <iostream>
+using namespace std;
+
+class Point {
+private:
+	float x, y;
+public:
+	Point();
+	float X() const;
+	float Y() const;
+	float distance(Point) const;
+	static float distance(Point, Point);
+	friend float distance(Point, Point);
+	friend bool operator==(Point, Point);
+	friend ostream& operator<<(ostream&, Point);
+};
