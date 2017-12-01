14309ed4f17b0dbf69ca485fc488c13e78a6e508
morrowind1983 <morrowind1983@gmail.com>
2017-12-01 13:54:59 +0800

    Update Ex_3_74

--- 02 Part Two - Data Structures/03 Chapter Three - Elementary Data Structures/Exercise_3_74.cpp
+++ 02 Part Two - Data Structures/03 Chapter Three - Elementary Data Structures/Exercise_3_74.cpp
@@ -20,34 +20,45 @@ struct node {
 
 typedef node *link;
 
-int linkNumber(int **adj, int V, int a, int b);
-int** malloc_random_adjacency_matrix(int V);
-void release_adjacency_matrix(int **adj, int V);
-void print_adjacency_matrix(int **adj, int V);
+int linkNumber(link* adj, int V, int a, int b);
+link* malloc_random_adjacency_list(int V);
+void release_adjacency_list(link* adj, int V);
+void print_adjacency_list(link* adj, int V);
 
 int main() {
-	int **matrix = malloc_random_adjacency_matrix(7);
-	print_adjacency_matrix(matrix, 7);
+	link* list = malloc_random_adjacency_list(7);
+	print_adjacency_list(list, 7);
 	
 	int a = 2;
 	int b = 3;
-	int cNumber = linkNumber(matrix, 7, a, b);
+	int cNumber = linkNumber(list, 7, a, b);
 	cout << "a = " << a << " b = " << b << " number of c = " << cNumber << endl;
 	
-	release_adjacency_matrix(matrix, 7);
+	release_adjacency_list(list, 7);
 
 	return 0;
 }
 
-int linkNumber(int **adj, int V, int a, int b) {
+int linkNumber(link* adj, int V, int a, int b) {
 	int count = 0;
-	for (int i = 0; i < V; i++) {
-		count += adj[a][i]*adj[i][b];
+	link x = adj[a];
+	link y = nullptr;
+	while (x != nullptr) {
+		int s = x->v;
+		y = adj[s];
+		while (y != nullptr) {
+			if (y->v == b) {
+				count++;
+			}
+			y = y->next;
+		}
+		x = x->next;
 	}
+
 	return count;
 }
 
-link* malloc_random_adjacency_matrix(int V) {
+link* malloc_random_adjacency_list(int V) {
 	link *adj = new link[V];
 	for (int i = 0; i < V; i++) {
 		adj[i] = nullptr;
@@ -67,29 +78,39 @@ link* malloc_random_adjacency_matrix(int V) {
 	return adj;
 }
 
-void release_adjacency_matrix(int **adj, int V) {
+void release_adjacency_list(link* adj, int V) {
+	link temp = nullptr;
 	for (int i = 0; i < V; i++) {
-		delete[] adj[i];
+		while (adj[i] != nullptr) {
+			temp = adj[i];
+			adj[i] = temp->next;
+			delete temp;
+			temp = nullptr;
+		}
 	}
 	delete[] adj;
 }
 
-void print_adjacency_matrix(int **adj, int V) {
+void print_adjacency_list(link* adj, int V) {
+	link x = nullptr;
 	for (int i = 0; i < V; i++) {
-		for (int j = 0; j < V; j++) {
-			cout << adj[i][j] << " ";
+		x = adj[i];
+		cout << "[" << i << "]";
+		while (x != nullptr) {
+			cout << x->v << " ";
+			x = x->next;
 		}
 		cout << endl;
 	}
 }
 
 /*
-1 0 1 0 0 1 1 
-1 1 0 1 0 0 0 
-0 1 0 0 1 0 1 
-0 0 0 0 0 0 0 
-0 0 0 1 0 1 0 
-0 0 0 0 0 0 0 
-0 0 1 0 0 0 1 
-a = 2 b = 3 number of c = 2
+[0]2 5 6 
+[1]3 0 
+[2]4 1 6 
+[3]
+[4]3 5 3 
+[5]
+[6]2 2 
+a = 2 b = 3 number of c = 3
 */
