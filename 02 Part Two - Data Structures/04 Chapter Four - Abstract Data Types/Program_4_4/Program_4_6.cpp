083bf15060027d0cf08a97a90ecaa15ea25ae352
Morrowind1983 <Morrowind1983@gmail.com>
2017-12-05 20:50:46 +0800

    Add Pro_4_6

---
+++ 02 Part Two - Data Structures/04 Chapter Four - Abstract Data Types/Program_4_4/Program_4_6.cpp
@@ -0,0 +1,31 @@
+/*
+Program 4.6. Infix-to-postfix conversion
+
+This program is another example of a pushdown-stack client. In this case, the
+stack contains characters. To convert (A+B) to the postfix form AB+, we ignore
+the left parenthesis, convert A to postfix, save the + on the stack, convert B
+to postfix, then, on encountering the right parenthesis, pop the stack and
+output the +.
+*/
+
+#include <iostream>
+#include "Stack.cpp"
+using namespace std;
+
+int main(int argc, char *argv[]) { 
+	char *a = argv[1];
+	int N = strlen(a);
+	Stack<char> ops(N);
+	for (int i = 0; i < N; i++) {
+		if (a[i] == ')') {
+			cout << ops.pop() << " ";
+		}
+		else if (a[i] == '+' || a[i] == '*') {
+			ops.push(a[i]);
+		}
+		else if (a[i] >= '0' && a[i] <= '9') {
+			cout << a[i] << " ";
+		}
+	}
+	cout << endl;
+}
